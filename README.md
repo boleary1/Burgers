3d67b2e8a917b770e2d43bb5aea601259b7a30b5
Brian OLeary <oleary.brian1@gmail.com>
2020-09-18 17:11:24 -0400

    no eat button on eaten butgers

--- README.md
+++ README.md
@@ -1,2 +1,30 @@
-# Burgers
-Burger Menu
+# Burger Menu
+## Live website URL:
+[live website](https://burger--menu.herokuapp.com/)
+</br>
+[GitHub Repository](https://github.com/boleary1/Burgers)
+</br>
+
+
+## **Description:**
+Application built in Mid September 2020.  Connects to a database you can add and update items in the database.  
+
+Last edit was 9/18/2020 by Brian O'leary.  Contact oleary.brian1@gmail.com with any questions.
+
+## **Screenshot:**
+
+![Screenshot of working app](Capture1.JPG)
+![Screenshot of database for the app](Capture2.JPG)
+
+
+## license:
+  MIT_License
+  
+  ## Contributing:
+  Yes, clone the repository make your edits and then create a push request to be reviewed.  
+
+  ## Testing:
+  No.
+
+## **Instructions:**
+Download the the repositoty to your local machine. create the database using the Schema file.  Open the file in your terminal.  Type "npm start" and follow the instructions.  
--- public/assets/js/burgers.js
+++ public/assets/js/burgers.js
@@ -1,6 +1,6 @@
 // Make sure we wait to attach our handlers until the DOM is fully loaded.
-$(function() {
-  $(".change-eaten").on("click", function(event) {
+$(function () {
+  $(".change-eaten").on("click", function (event) {
     var id = $(this).data("id");
     var nowEaten = 1;
 
@@ -14,7 +14,7 @@ $(function() {
       type: "PUT",
       data: nowEatenState
     }).then(
-      function() {
+      function () {
         console.log("changed eaten to", nowEaten);
         // Reload the page to get the updated list
         location.reload();
@@ -22,7 +22,7 @@ $(function() {
     );
   });
 
-  $(".create-form").on("submit", function(event) {
+  $(".create-form").on("submit", function (event) {
     // Make sure to preventDefault on a submit event.
     event.preventDefault();
 
@@ -32,26 +32,29 @@ $(function() {
     };
 
     // Send the POST request.
-    $.ajax("/api/burgers", {
-      type: "POST",
-      data: newBurger
-    }).then(
-      function() {
-        console.log("created new burger");
-        // Reload the page to get the updated list
-        location.reload();
-      }
-    );
+    if (newBurger.name) {
+      $.ajax("/api/burgers", {
+        type: "POST",
+        data: newBurger
+      }).then(
+        function () {
+          console.log("created new burger");
+          // Reload the page to get the updated list
+          location.reload();
+        }
+      );
+    };
   });
 
-  $(".delete-burger").on("click", function(event) {
+
+  $(".delete-burger").on("click", function (event) {
     var id = $(this).data("id");
 
     // Send the DELETE request.
     $.ajax("/api/burgers/" + id, {
       type: "DELETE"
     }).then(
-      function() {
+      function () {
         console.log("deleted burger", id);
         // Reload the page to get the updated list
         location.reload();
