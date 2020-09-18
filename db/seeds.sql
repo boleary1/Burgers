78441d43b321ce9b3337f64cb73f8936faaa1fca
Brian OLeary <oleary.brian1@gmail.com>
2020-09-18 14:08:07 -0400

    update and delete are working

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,6 +1,2 @@
-INSERT INTO cats (name) VALUES ('Meeses');
-INSERT INTO cats (name) VALUES ('Bobbi');
-INSERT INTO cats (name, sleepy) VALUES ('Sylvester', true);
-INSERT INTO cats (name, sleepy) VALUES ('Marilyn Manson', true);
-INSERT INTO cats (name, sleepy) VALUES ('Joe Biden', true);
-INSERT INTO cats (name) VALUES ('Owens');
+INSERT INTO burgers (name) VALUES ('Hamburger');
+INSERT INTO burgers (name, eaten) VALUES ('Cheese Burger', true);
